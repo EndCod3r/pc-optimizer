fca2249fc5dc7c911f86b6ab95535ea54bbe61c7
EndCod3r <endlord15@proton.me>
2024-07-27 02:25:48 -0800

    Added remove telemetray function

--- optimize.ps1
+++ optimize.ps1
@@ -115,3 +115,53 @@ if ( $userinput -eq 'y' -or !$userinput ) {
     Write-Output "Running DISM Image Cleanup"
     Dism.exe /Online /Cleanup-Image /StartComponentCleanup /ResetBase
 }
+
+if ($AcceptRecommendedTweaks) {
+    $userinput = 'y'
+} else {$userinput = Read-Host -Prompt "Do you want to disable Microsoft's telemetry? (Recommnded) (Y/n)"}
+if ($userinput -eq 'y' -or !$userinput) {
+    # this is just taken from ChrisTitusTech's winutil because I don't understand any of this
+
+    bcdedit /set {current} bootmenupolicy Legacy | Out-Null
+      If ((Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name CurrentBuild).CurrentBuild -lt 22557) {
+          $taskmgr = Start-Process -WindowStyle Hidden -FilePath taskmgr.exe -PassThru
+          Do {
+              Start-Sleep -Milliseconds 100
+              $preferences = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\TaskManager" -Name "Preferences" -ErrorAction SilentlyContinue
+          } Until ($preferences)
+          Stop-Process $taskmgr
+          $preferences.Preferences[28] = 0
+          Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\TaskManager" -Name "Preferences" -Type Binary -Value $preferences.Preferences
+      }
+      Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}" -Recurse -ErrorAction SilentlyContinue
+
+      # Fix Managed by your organization in Edge if regustry path exists then remove it
+
+      If (Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge") {
+          Remove-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Recurse -ErrorAction SilentlyContinue
+      }
+
+      # Group svchost.exe processes
+      $ram = (Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1kb
+      Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control" -Name "SvcHostSplitThresholdInKB" -Type DWord -Value $ram -Force
+
+      $autoLoggerDir = "$env:PROGRAMDATA\Microsoft\Diagnosis\ETLLogs\AutoLogger"
+      If (Test-Path "$autoLoggerDir\AutoLogger-Diagtrack-Listener.etl") {
+          Remove-Item "$autoLoggerDir\AutoLogger-Diagtrack-Listener.etl"
+      }
+      icacls $autoLoggerDir /deny SYSTEM:`(OI`)`(CI`)F | Out-Null
+
+      # Disable Defender Auto Sample Submission
+      Set-MpPreference -SubmitSamplesConsent 2 -ErrorAction SilentlyContinue | Out-Null
+
+      # Disables Scheduled Tasks
+      $tele = Get-Content .\Config\tele.json | ConvertFrom-Json
+
+      for ($i = 0; $i -lt $tele.ScheduledTask.name.Count; $i++) {
+        Disable-ScheduledTask -TaskName $tele.ScheduledTask.name[$i] -ErrorAction SilentlyContinue
+
+    }
+    for ($i = 0; $i -lt $tele.registry.name.Count; $i++) {
+        Set-ItemProperty -Path $tele.registry.Path[$i] -Name $tele.registry.Name[$i] -Value $tele.registry.Value[$i] -Type $tele.registry.Type[$i] 
+    }
+}
