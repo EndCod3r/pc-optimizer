1637dacbcf93f15af0fd99e45037651133c52e7e
EndCod3r <endlord15@proton.me>
2024-07-27 17:05:49 -0800

    fixed grammar

--- optimize.ps1
+++ optimize.ps1
@@ -19,7 +19,7 @@ if ($AcceptAllTweaks) {
     $AcceptRecommendedTweaks = $true
     Read-Host -Prompt "
     You gave the parameter to accept all tweaks.
-    This is HIGHLY unrecommended if you don't know what all of the tweaks.
+    This is HIGHLY unrecommended if you don't know what all of the tweaks do.
     Press enter to continue or CTRL+C to exit."
 }
 
